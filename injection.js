fb405a11ab77d1f2f197d4da8703131e83a9617b
Rice <108159282+CynosaHacks@users.noreply.github.com>
2024-11-26 19:05:24 +1100

    Update injection.js

--- injection.js
+++ injection.js
@@ -308,7 +308,6 @@ function modifyCode(text) {
 	addReplacement('this.game.info.showSignEditor=null,exitPointerLock())', `
 		if (this.showDeathScreen && enabledModules["AnticheatDisabler"]) {
 			ClientSocket.sendPacket(new SPacketRespawn$1);
-   			new Module("https://github.com/CynosaHacks/VapeForMiniblox/", function() {});
 		}
 	`);
 
@@ -762,10 +761,6 @@ function modifyCode(text) {
 			new Module("InvWalk", function() {});
 			new Module("KeepSprint", function() {});
 			new Module("NoSlowdown", function() {});
-   			new Module("AdminPermissions", function() {});
-      			new Module("FloatBypassCheck", function() {});
-	 		new Module("FireballSpeedJump", function() {});
-    
 
 			// NoFall
 			new Module("NoFall", function(callback) {
@@ -781,10 +776,7 @@ function modifyCode(text) {
 				}
 				else delete tickLoop["NoFall"];
 			});
-			game$1.chat.addChat({
-  				text: "Anticheat Disabled!",
-  				color: "red" // Use a color to indicate disabled state (optional)
-			});
+
 			// Speed
 			let speedvalue, speedjump, speedauto;
 			const speed = new Module("Speed", function(callback) {
@@ -1298,7 +1290,7 @@ function modifyCode(text) {
 			saveVapeConfig();
 		}, 10000);
 	}
-	
+
 	const publicUrl = "scripturl";
 	// https://stackoverflow.com/questions/22141205/intercept-and-alter-a-sites-javascript-using-greasemonkey
 	if (publicUrl == "scripturl") {
